00c48a310954badcfcbd5ceb1a7f729ab52a593c
xiaoAnn <AnFV_97@163.com>
2019-02-21 11:03:23 +0800

    6

--- mina/app.js
+++ mina/app.js
@@ -6,7 +6,7 @@ App({
         userInfo: null,
         version: "1.0",
         shopName: "Python3 + Flask 订餐全栈系统",
-        domain:"http://192.168.0.119:8999/api"
+        domain:"http://192.168.43.131:8999/api"
     },
     tip:function( params ){
         var that = this;
@@ -56,5 +56,16 @@ App({
         return {
             'content-type': 'application/x-www-form-urlencoded'
         }
+    },
+    buildUrl:function(path, params){
+      var url = this.globalData.domain + path;
+      var _paramUrl = "";
+      if (params) {
+        _paramUrl = Object.keys(params).map(function (k) {
+          return [encodeURIComponent(k), encodeURIComponent(params[k])].join("=");
+        }).join("&");
+        _paramUrl = "?" + _paramUrl;
+      }
+      return url + _paramUrl;
     }
 });
--- mina/pages/index/index.js
+++ mina/pages/index/index.js
@@ -5,7 +5,8 @@ Page({
   data: {
     remind: '加载中',
     angle: 0,
-    userInfo: {}
+    userInfo: {},
+    regFlag:true
   },
   goToIndex:function(){
     wx.switchTab({
@@ -15,7 +16,9 @@ Page({
   onLoad:function(){
     wx.setNavigationBarTitle({
       title: app.globalData.shopName
-    })
+    });
+    this. checkLogin()
+    
   },
   onShow:function(){
 
@@ -37,5 +40,60 @@ Page({
         });
       }
     });
+  },
+  checkLogin:function(){
+    wx.login({
+      success: function (res) {
+        if (!res.code) {
+          app.alert({ 'content': '登录失败，请再次点击~~' });
+          return;
+        }
+        wx.request({
+          url: app.buildUrl('/member/check-reg'),
+          header: app.getRequestHeader(),
+          method: 'POST',
+          data: { code: res.code },
+          success: function (res) {
+            if (res.data.code != 200) {
+              that.setData({
+                regFlag: false
+              });
+              return;
+            }
+
+            app.setCache("token", res.data.data.token);
+            //that.goToIndex();
+          }
+        });
+      }
+    });
+  },
+
+  login:function(e){
+    if(!e.detail.userInfo){
+      app.alert({'content':'登录失败,再次点击'});
+      return;
+    }
+
+    var data = e.detail.userInfo
+    wx.login({
+      success:function(res){
+        if (!res.code){
+          app.alert({'content':'登录失败'})
+          return;
+        }
+        data['code'] = res.code;
+        wx.request({
+          url: app.buildUrl('/member/login'),
+          header: app.getRequestHeader(),
+          method: 'POST',
+          data: {code:res.code},
+          success: function (res) {
+
+          }
+        })
+      }
+    })
+    
   }
 });
