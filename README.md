0a0c07bbf116670b11a3c10e0ebe3669d0268dae
xiaoAnn <AnFV_97@163.com>
2019-02-22 12:01:20 +0800

    10

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 
     flask-sqlacodegen 'mysql+pymysql://root:123456@127.0.0.1/food_db' --outfile "common/models/model.py"  --flask
     flask-sqlacodegen 'mysql+pymysql://root:123456@127.0.0.1/food_db' --tables user --outfile "common/models/user.py"  --flask
+    flask-sqlacodegen 'mysql+pymysql://root:123456@127.0.0.1/food_db' --tables food --outfile "common/models/food/Food.py"  --flask
+    flask-sqlacodegen 'mysql+pymysql://root:123456@127.0.0.1/food_db' --tables food_cat --outfile "common/models/food/FoodCat.py"  --flask
+    flask-sqlacodegen 'mysql+pymysql://root:123456@127.0.0.1/food_db' --tables food_sale_change_log --outfile "common/models/food/FoodSaleChangeLog.py"  --flask
+    flask-sqlacodegen 'mysql+pymysql://root:123456@127.0.0.1/food_db' --tables food_stock_change_log --outfile "common/models/food/FoodStockChangeLog.py"  --flask
--- web/controllers/food/Food.py
+++ web/controllers/food/Food.py
@@ -1,29 +1,66 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint,render_template
-
+from flask import Blueprint, request, jsonify
+from common.libs.Helper import ops_render
+from common.models.food.FoodCat import FoodCat
+from common.libs.Helper import getCurrentData
+from application import db
 route_food = Blueprint( 'food_page',__name__ )
-
 
 @route_food.route( "/index" )
 def index():
-    return render_template( "food/index.html" )
+    return ops_render( "food/index.html" )
 
 
 @route_food.route( "/info" )
 def info():
-    return render_template( "food/info.html" )
+    return ops_render( "food/info.html" )
 
 
 @route_food.route( "/set" )
 def set():
-    return render_template( "food/set.html" )
+    return ops_render( "food/set.html" )
 
 
 @route_food.route( "/cat" )
 def cat():
-    return render_template( "food/cat.html" )
+    
+    return ops_render( "food/cat.html" )
 
 
-@route_food.route( "/cat-set" )
+@route_food.route( "/cat-set",methods = [ "GET","POST" ] )
 def catSet():
-    return render_template( "food/cat_set.html" )
+    if request.method == "GET":
+        resp_data = {}
+        req = request.args
+        id = int(req.get("id", 0))
+        info = None
+        if id:
+            info = FoodCat.query.filter_by( id = id ).first()
+        resp_data['info'] = info
+        resp_data['current'] = 'cat'
+        return ops_render( "food/cat_set.html" ,resp_data )
+
+    resp = {'code': 200, 'msg': '操作成功~~', 'data': {}}
+    req = request.values
+
+    id = req['id'] if 'id' in req else 0
+    name = req['name'] if 'name' in req else ''
+    weight = int( req['weight'] ) if ( 'weight' in req  and  int( req['weight']) > 0 ) else 1
+
+    if name is None or len( name ) < 1:
+        resp['code'] = -1
+        resp['msg'] = "请输入符合规范的分类名称~~"
+        return jsonify( resp )
+
+    food_cat_info = FoodCat.query.filter_by( id = id ).first()
+    if food_cat_info:
+        model_food_cat = food_cat_info
+    else:
+        model_food_cat = FoodCat()
+        model_food_cat.created_time = getCurrentData()
+    model_food_cat.name = name
+    model_food_cat.weight = weight
+    model_food_cat.updated_time = getCurrentData()
+    db.session.add(model_food_cat)
+    db.session.commit()
+    return jsonify( resp )
